8ce2c50e833e8f98ab48fdc36a9c87edd0daf9d7
Feranmi Ade <42687078+techiefer@users.noreply.github.com>
2019-12-14 01:22:26 +0100

    added some basic files

---
+++ js/validation.js
@@ -0,0 +1,10 @@
+"use strict"
+const inputtedName = document.querySelector('.name');
+
+function formvalidation() {
+    const checkName = (inputtedName, ) => {
+        if (inputtedName == null || inputtedName == undefined) {
+            
+        }
+    }
+}
